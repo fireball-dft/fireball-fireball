9b2725f434d21ff0087ecaa285ce5cf18e1026f2
Guillermo <gufranco@mail.wvu.edu>
2016-05-23 14:06:41 -0400

    Changing links to master source

--- src/runme_before_compile.sh
+++ src/runme_before_compile.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+MASTER_PATH=fireball-master
+
 for i in a.GLOBAL b.FUNCTIONS c.SYSTEM d.FUNCTIONS_EXTRA e.FDATA g.XC_FUNCTIONALS h.SOLVESH i.GRID j.ASSEMBLERS k.DASSEMBLERS l.SCF m.MD n.NAC p.HARRIS q.DOGS o.OUTPUT u.UTIL include libs Makefile MACHINES
 do
-    if [ ! -e $i ]
+    if [ -h $i ]
     then
-	ln -s ../../fireball-thunder/src/$i
+	rm $i
     fi
+    ln -s ../../${MASTER_PATH}/src/$i
 done
